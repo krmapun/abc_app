87cf1c8b5287d12c2586b70b432e0c8be9aa9939
AbcGrupo <abcaeroapp@gmail.com>
2019-07-10 21:33:15 -0500

    ios publicacion

--- appAbc/src/app/app.module.ts
+++ appAbc/src/app/app.module.ts
@@ -9,7 +9,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-import { firebaseConfig } from '../environments/environment';
+// import { firebaseConfig } from '../environments/environment';
+import { firebaseConfig } from '../environments/environment.prod';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 
--- appAbc/src/environments/environment.prod.ts
+++ appAbc/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true
 };
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDtKs_LuE94lZORli3sagbycd39jOhxKnA',
+  authDomain: 'appabcgroup.firebaseapp.com',
+  databaseURL: 'https://appabcgroup.firebaseio.com',
+  projectId: 'appabcgroup',
+  storageBucket: 'appabcgroup.appspot.com',
+  messagingSenderId: '385413999655',
+  appId: '1:385413999655:web:e18218c6877ba6c6'
+};
+
